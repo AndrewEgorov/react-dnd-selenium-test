a55fafdcf25e868d4796e895bc099d071ee87be0
David Collard <david.collard@cancer.org.uk>
2018-05-31 16:57:02 +0100

    Add a POC Java application that uses Selenium with a JavaScript addition to workaround the broken HTML5 drag and drop

---
+++ fruit-basket-test/src/FruitBasketTest.java
@@ -0,0 +1,57 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class FruitBasketTest {
+    public static void main(String[] args) {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://crukorg.github.io/react-dnd-selenium-test/");
+
+        // Load jQuery and the JS script workaround for HTML5 drag and drop
+        driver.executeScript(loadJavascript("resources/jquery-3.3.1.min.js"));
+        driver.executeScript(loadJavascript("resources/dragdrop.js"));
+
+        // Simulate dragging the apple to the box
+        driver.executeScript("$('.apple').simulateDragDrop({dropTarget: '.box'})");
+
+        // Get the output message and print it
+        WebElement type = driver.findElementByClassName("app--type");
+        String text = type.getText();
+        System.out.println("Message: " + text);
+    }
+
+    private static String loadJavascript(String resource) {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream stream = classloader.getResourceAsStream(resource);
+        BufferedReader reader = null;
+        String line;
+        StringBuffer buffer = new StringBuffer();
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + '\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return buffer.toString();
+    }
+}
